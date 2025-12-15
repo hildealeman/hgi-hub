0702e45e0b11656a08abe322bf65d78fd893aed8
hildealeman <37812451+hildealeman@users.noreply.github.com>
2025-12-15 02:19:46 -0600

    add HGI Genesis module (server page + thread forum)

---
+++ src/app/genesis/thread/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { ThreadDiscussion } from "@/components/ThreadDiscussion";
+
+export default function GenesisThreadPage() {
+  return (
+    <ThreadDiscussion 
+      section="genesis"
+      sectionTitle="HGI Genesis"
+    />
+  );
+}
